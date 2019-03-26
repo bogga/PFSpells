ad15f2b336823d55b14d6baddc2a55746a509148
Adam Bogg <adbogg+git@gmail.com>
2019-03-26 18:56:49 +0000

    Refactor to use HttpClient for searchDoc

--- PFSpells/Form1.cs
+++ PFSpells/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -35,7 +36,7 @@ namespace PFSpells
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        async private void button1_Click(object sender, EventArgs e)
         {
             button1.Enabled = false;
             string[] spellNames = textBox1.Lines;
@@ -62,12 +63,13 @@ namespace PFSpells
                 {
                     nameForURL = Regex.Replace(name, "[^0-9a-zA-Z' ]", "");
                     url = "https://cse.google.com/cse?cx=006680642033474972217%3A6zo0hx_wle8&q=" + nameForURL + "#gsc.tab=0&gsc.q=" + nameForURL + "&gsc.ref=more%3Aspells&gsc.sort=";
-                    WebBrowser wb = new WebBrowser();
-                    wb.Navigate(url);
-                    while (wb.ReadyState != WebBrowserReadyState.Complete)
-                        Application.DoEvents();
+
+                    HttpClient client = new HttpClient();
+                    string body = await client.GetStringAsync(url);
+
                     HtmlAgilityPack.HtmlDocument searchDoc = new HtmlAgilityPack.HtmlDocument();
-                    searchDoc.LoadHtml(wb.DocumentText);
+                    searchDoc.LoadHtml(body);
+
                     searchDoc.Save("./html.html");
                     HtmlNode linkNode = searchDoc.DocumentNode.SelectSingleNode("//a[contains(@class, 'gs-title')]");
                     htmlDoc = web.LoadFromBrowser(url);

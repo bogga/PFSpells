1ce2235e1bdc82a05a3b6e3e778478856c004e5a
Adam Bogg <adbogg+git@gmail.com>
2019-04-10 20:40:14 +0100

    Stopped resizing of window (it looks bad)

--- PFSpells/GeneratorWindow.Designer.cs
+++ PFSpells/GeneratorWindow.Designer.cs
@@ -206,6 +206,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GeneratorWindow";
             this.Text = "Spell List Generator";
